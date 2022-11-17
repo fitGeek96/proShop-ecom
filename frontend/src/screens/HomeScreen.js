368f219316fa54a42f2e3255375ef9f6af15da0e
fitgeek96 <fitgeek96@gmail.com>
2022-11-17 11:21:23 +0200

    Add Rating Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,8 +9,8 @@ export const HomeScreen = () => {
       <h2>Latest Products</h2>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
+          <Col sm={12} md={6} lg={4}>
+            <Product product={product} key={product._id} />
           </Col>
         ))}
       </Row>
